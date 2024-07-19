c057d34da3bc93faf9e1ecf4e693c66f8eb6281b
EmanuelLicona <emanuel.licona@gmail.com>
2024-07-18 22:30:06 -0600

    Agregando las clases para ejecutar con intervalo de tiempo o cron

---
+++ ScheduledTasksCindy/Helpers/EmailEmployeesJob.cs
@@ -0,0 +1,29 @@
+﻿using Quartz;
+using ScheduledTasksCindy.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ScheduledTasksCindy.Helpers
+{
+    public class EmailEmployeesJob : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            return Task.Run(() =>
+            {
+                try
+                {
+                    BaseController.EnviarCorreo();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error al enviar el correo: " + ex.Message);
+                }
+            });
+        }
+    }
+}
---
+++ ScheduledTasksCindy/Helpers/JobScheduler.cs
@@ -0,0 +1,44 @@
+﻿using Quartz.Impl;
+using Quartz;
+
+namespace ScheduledTasksCindy.Helpers
+{
+    public class JobScheduler
+    {
+        public static void Start()
+        {
+            IScheduler scheduler = new StdSchedulerFactory().GetScheduler().Result;
+            scheduler.Start().Wait();
+
+            IJobDetail job = JobBuilder
+                .Create<EmailEmployeesJob>()
+                .WithIdentity("email_empleados")
+                .Build();
+
+            ITrigger trigger = TriggerBuilder.Create()
+            .WithIdentity("trigger1", "group1")
+            .StartNow()
+            .WithSimpleSchedule(x => x
+                .WithIntervalInSeconds(10)
+                .RepeatForever()
+            )
+            .Build();
+
+            //// Definir el primer trigger (8:00 AM)
+            //ITrigger trigger1 = TriggerBuilder.Create()
+            //    .WithIdentity("trigger1", "group1")
+            //    .WithCronSchedule("0 0 8 * * ?") // Ejecuta a las 8:00 AM todos los días
+            //    .ForJob(job)
+            //    .Build();
+
+            //// Definir el segundo trigger (4:00 PM)
+            //ITrigger trigger2 = TriggerBuilder.Create()
+            //    .WithIdentity("trigger2", "group1")
+            //    .WithCronSchedule("0 0 16 * * ?") // Ejecuta a las 4:00 PM todos los días
+            //    .ForJob(job)
+            //    .Build();
+
+            scheduler.ScheduleJob(job, trigger);
+        }
+    }
+}
